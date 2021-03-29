4adcca483e78734423d666772640dc985ae52cf2
Oscar Hernandez <oh@wzrdgroup.com>
2021-03-29 10:18:20 -0400

    cookies

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,27 +29,35 @@ $( ".toggle-color" ).click(function() {
 });
 
 $(document).ready(function(){  
+  
 
-  var $form = $('form');  
+  $("#mc-embedded-subscribe-form").submit(function (event){
+    event.preventDefault();
+    function isValidEmailAddress(emailAddress) {
+      var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+      return pattern.test(emailAddress);
+    }
 
-  $( "input" ).focus(function() {
-    $("#button").addClass("make-visible");
-  })
-
-  $form.submit(function(){
-    $("button").addClass("is-done");
-    $("button").addClass("is-active");
-    $("button").text("Subscribed");
-    $("button").css("cursor","default");
-    $("#button").css("opacity","1");
-    console.log("submitted");
-     $.post($(this).attr('action'), $(this).serialize(), function(response){
-           // do something here on success
-           console.log("did it");           
-     },'json');
-     return false;
+    if (isValidEmailAddress($("#mce-EMAIL").val())){
+      $(".button").addClass("showButton");
+      $(".clear").addClass("w-100");
+      $(".button").addClass("submittedButton");
+      $(".button").attr("value","Submitted");
+    }        
   });
 
 });
 
-
+$(document).ready(function(){  
+  $( "input" ).focus(function() {
+    $("label").addClass("moveLabel");
+    $(".button").addClass("showButton")
+    // $( "label" ).animate({      
+    // }, 1000, function() {
+    // });
+  });
+  $( "input" ).blur(function() {
+    $("label").removeClass("moveLabel");
+    $(".button").removeClass("showButton")
+  });
+});
